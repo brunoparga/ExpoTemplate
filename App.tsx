862a53be8af2b8a47407163158d54c21ace8b385
Bruno Parga <brunoparga@gmail.com>
2021-01-29 19:01:00 +0300

    Add ESLint, Jest and Elements + config

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +10,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function App(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// Used only in the test and internally by Expo
+// eslint-disable-next-line import/no-unused-modules
+export default App;
